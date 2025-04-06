509408f62609bdee5586a6ea3d999059b7f69fcc
pankaj28chauhan <62992713+pankaj28chauhan@users.noreply.github.com>
2025-04-06 17:38:02 +0000

    changs functions

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,10 +24,10 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+ //const obj3 = Object.assign({}, obj1, obj2, obj4)
 
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+//const obj3 = {...obj1, ...obj2}
+ //console.log(obj3);
 
 
 const users = [
